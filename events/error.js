84590d2f2034e226c38525a09c3e1f4d8c30e2a0
Tails <82265690+PikachuTW@users.noreply.github.com>
2022-03-10 19:42:50 +0800

    20220310 Update

--- events/error.js
+++ events/error.js
@@ -1,4 +1,4 @@
-const logger = require('../modules/Logger.js');
+const logger = require('../modules/logger.js');
 module.exports = async (client, error) => {
     logger.error(`${JSON.stringify(error)}`);
 };
