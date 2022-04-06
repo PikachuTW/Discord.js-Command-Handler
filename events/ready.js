18bd140eb50573aed6276a82ee62f29469420225
Tails <82265690+PikachuTW@users.noreply.github.com>
2022-04-06 10:10:18 +0800

    update

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ const logger = require('../modules/logger.js');
 const { settings } = require('../config.js');
 
 module.exports = async (client) => {
-    logger.ready(`${client.user.tag}, 成員數: ${client.guilds.cache.map((g) => g.memberCount).reduce((a, b) => a + b)} ，伺服器數: ${client.guilds.cache.size}`);
+    logger.ready(`${client.user.tag}, 成員數: ${client.guilds.cache.map((g) => g.memberCount).reduce((a, b) => a + b, 0)} ，伺服器數: ${client.guilds.cache.size}`);
     if (settings.activity) {
         client.user.setActivity(settings.activity, { type: 'PLAYING' });
     }
